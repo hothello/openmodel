8a5b86dd53dff7d3b01c8adc41cbb46833b7db8f
HotHello <roscioni@gmail.com>
2024-12-13 11:34:02 +0000

    Migration to MOLC 2024, part two.#

--- multiscale/05_postprocess.sh
+++ multiscale/05_postprocess.sh
@@ -23,12 +23,12 @@ perl ../scripts/dumptools.pl -f last -w $cg_dump
 
 # 2. Patch the CG polymer slab, introducing separate head and tail residues in post-processing.
 tmp="tmp_cg_sample"
-perl ../scripts/dumptools.pl -p 1 -p 2 -o $tmp ${cg_dump/.dump/}_f*dump
+perl ../scripts/dumptools.pl -p 1 -p 2 -o $tmp -f 1 ${cg_dump/.dump/}_t*dump
 
 # 3. Back-map the CG sample to atomic coordinates.
 # NOTE: the order of the atomic coordinates in XYZ files, must reflect the atom types
 # in the patched DUMP file containing the CG sample.
-backmap_legacy -t $plga/lac.xyz -t $plga/gly.xyz -t $wat/tip4p.xyz \
+backmap -t $plga/lac.xyz -t $plga/gly.xyz -t $wat/tip4p.xyz \
 -t $plga/lac1.xyz -t $plga/lac2.xyz -t $plga/gly1.xyz -t $plga/gly2.xyz \
 -o $output $tmp.dump
 mv -f $output.????* $output.pdb
@@ -36,7 +36,7 @@ mv -f $output.????* $output.pdb
 # 4.1 Render the input script with MOLTEMPLATE.
 molecules=$(grep new ${cg_dump/.dump/.lt} | awk '$0~/=/{printf "%s\\n",$0}')
 sed -e 's/REPLACE_MOLECULES/'"$molecules"'/' $lt_template > $lt_input
-moltemplate.sh -overlay-bonds -overlay-angles -overlay-dihedrals -overlay-impropers -pdb $output.pdb $lt_input
+moltemplate.sh -overlay-all -pdb $output.pdb $lt_input
 
 # 4.2 Create the structure file (PSF) to read the DCD file.
 # NOTE: requires VMD.
